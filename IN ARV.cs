e0326c15ded442215eebf42568c377daaa2c806b
Rahelaraya <rahelaraya26@gmail.com>
2024-09-13 02:37:01 +0200

    Add files via upload

---
+++ IN ARV.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace IN_1
+{
+    internal interface IN_ARV
+    {
+    }
+
+    public class Drink
+    {
+        // Attributer
+
+        public string Name;
+
+        public string Test;
+
+        // Metoder
+
+        public void sup()
+        {
+            Console.WriteLine(" this drink is Water");
+
+        }
+        public void sul()
+        {
+            Console.WriteLine("Water test nothing");
+        }
+    }
+
+
+    public class Juice : Drink
+    {
+        // Attributer
+
+        public string fruit;
+
+        public string extracted;
+
+        // Metoder
+
+        public void dup()
+        {
+            Console.WriteLine(" this drink is Juice");
+
+        }
+        public void dul()
+        {
+            Console.WriteLine("Juice test sweet");
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IN_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ARV INL 1
+
+            Drink drink1 = new Drink();
+
+            drink1.sup();
+            drink1.sul();
+
+
+            Juice Juice1 = new Juice();
+
+            Juice1.dup();
+            Juice1.dul();
+
+        }
+    }
+}
